3e65217859f5511e3e756d2faeccf97e76885302
Syed Ateeq <Syedateeq160@gmail.com>
2021-04-13 23:42:33 +0500

    updated bootstrap

--- src/App.js
+++ src/App.js
@@ -32,30 +32,31 @@ function App()
 		<div className="calc-app">
 			<img src="/images/1.svg"></img>
 			<from>
-				<input type="text" value={result} ref={inputRef} />
+				<input class="form-control" id="focusedInput" type="text" value={result} ref={inputRef} />
 			</from>
+			
 
 			<div className="keypad">
-				<button id="clear" onClick={clear}>Clear</button>
-				<button id="backspace" onClick={backspace}>C</button>
+				<button class="btn btn-danger" id="clear" onClick={clear}>Clear</button>
+				<button class="btn btn-warning"id="backspace" onClick={backspace}>C</button>
 
-				<button name="+" onClick={handleClick}>+</button>
-				<button name="7" onClick={handleClick}>7</button>
-				<button name="8" onClick={handleClick}>8</button>
-				<button name="9" onClick={handleClick}>9</button>
-				<button name="-" onClick={handleClick}>-</button>
-				<button name="4" onClick={handleClick}>4</button>
-				<button name="5" onClick={handleClick}>5</button>
-				<button name="6" onClick={handleClick}>6</button>
-				<button name="*" onClick={handleClick}>&times;</button>
-				<button name="1" onClick={handleClick}>1</button>
-				<button name="2" onClick={handleClick}>2</button>
-				<button name="3" onClick={handleClick}>3</button>
-				<button name="/" onClick={handleClick}>/</button>
-				<button name="0" onClick={handleClick}>0</button>
-				<button name="." onClick={handleClick}>.</button>
+				<button class="btn btn-primary"name="+" onClick={handleClick}>+</button>
+				<button class="btn btn-secondary"name="7" onClick={handleClick}>7</button>
+				<button class="btn btn-secondary"name="8" onClick={handleClick}>8</button>
+				<button class="btn btn-secondary"name="9" onClick={handleClick}>9</button>
+				<button class="btn btn-primary"name="-" onClick={handleClick}>-</button>
+				<button class="btn btn-secondary"name="4" onClick={handleClick}>4</button>
+				<button class="btn btn-secondary"name="5" onClick={handleClick}>5</button>
+				<button class="btn btn-secondary"name="6" onClick={handleClick}>6</button>
+				<button class="btn btn-primary"name="*" onClick={handleClick}>&times;</button>
+				<button class="btn btn-secondary"name="1" onClick={handleClick}>1</button>
+				<button class="btn btn-secondary"name="2" onClick={handleClick}>2</button>
+				<button class="btn btn-secondary"name="3" onClick={handleClick}>3</button>
+				<button class="btn btn-primary"name="/" onClick={handleClick}>/</button>
+				<button class="btn btn-secondary"name="0" onClick={handleClick}>0</button>
+				<button class="btn btn-secondary"name="." onClick={handleClick}>.</button>
 				
-				<button variant="success" id="result" onClick={calculate}>Result</button>
+				<button className="btn btn-success" id="result" onClick={calculate}>Result</button>
 				
 			</div>
 		</div>
